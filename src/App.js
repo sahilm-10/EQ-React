ea365ba980c4dd669fcf5f3efdc84ecfb15af6ff
Sahil Anand Malgundkar <samalgundkar@ivp.co.in>
2023-09-01 16:38:52 +0530

    Class Component Example Added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
+import WelcomeMessage from './Components/FirstClassComponent';
 import GreetMessage from './Components/FirstFunctionalComponent';
 import FullName from './Components/JsxExample';
 
 function App() {
   return (
     <div className="App">
-      <FullName/>
+      <WelcomeMessage/>
     </div>
   );
 }
---
+++ src/Components/FirstClassComponent.js
@@ -0,0 +1,10 @@
+import React,{Component} from "react";
+
+class WelcomeMessage extends Component{
+    // Class Components are Statefull,Smart and they have Lifecycle methods as compared to FC.
+    render(){
+        return <h1>This is a Class Component</h1>
+    }
+}
+
+export default WelcomeMessage;
