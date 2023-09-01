6844b90b2922ae9d4821062c5976aa5e5536f6c6
Sahil Anand Malgundkar <samalgundkar@ivp.co.in>
2023-09-01 15:47:24 +0530

    Added First Functional Component

---
+++ src/Components/FirstFunctionalComponent.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+// Functional Component -> Nothing just a Function which optionally takes inputs as properties(props) and returns a JSX.
+
+function GreetMessage(){
+    return <h1>This is a Functional Component</h1>
+}
+
+export default GreetMessage;
