db13d1f25faa502fc633bdf5fd5b3250d74d14e2
Sahil Anand Malgundkar <samalgundkar@ivp.co.in>
2023-09-05 18:51:17 +0530

    ConditionalUseEffect Example Added

--- src/Hooks/useEffectExample.js
+++ src/Hooks/useEffectExample.js
@@ -4,12 +4,15 @@ import React, { useEffect, useState } from "react";
 // useEffect  runs after every render !!!
 function ClickCounter(){
     const [count,setCount] = useState(0);
+    const [name,setName] = useState('');
     useEffect(()=>{
         console.log("Inside Hook!")
         document.title = `You Have Clicked Me ${count} times!`
-    })
+    },[count])
+    // Here useEffect will only be called when there is a change in count, This is Conditionally running the useEffect!
     return(
         <div>
+            <input type="text" placeholder="Type here"/>
             <button onClick={()=>setCount(count + 1)}>Count:{count}</button>
         </div>
     )
