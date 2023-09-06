5c6961a9fa1b7daaa0a9040bed0e127691f3de6c
Sahil Anand Malgundkar <samalgundkar@ivp.co.in>
2023-09-06 15:13:19 +0530

    Added Button For FetchingData

--- src/Hooks/DataFetchById.js
+++ src/Hooks/DataFetchById.js
@@ -3,9 +3,13 @@ import axios from "axios";
 function GetById(){
     const[post,setPost] = useState({});
     const[id,setId] = useState(1);
+    const[idFromBtnClick,setIdFromBtnClick] = useState(1);
 
+    const FetchPost = () =>{
+        setIdFromBtnClick(id);
+    }
     useEffect(()=>{
-        axios.get(`https://jsonplaceholder.typicode.com/posts/${id}`)
+        axios.get(`https://jsonplaceholder.typicode.com/posts/${idFromBtnClick}`)
             .then(res => {
                 console.log(res);
                 setPost(res.data)
@@ -13,10 +17,11 @@ function GetById(){
             .catch(error =>{
                 console.log(error);
             })
-    },[id])
+    },[idFromBtnClick])
     return(
         <div>
             <input type="text" value={id} onChange={e => setId(e.target.value)}/>
+            <button onClick={FetchPost}>Fetch Post</button>
             <h2>{post.title}</h2>
         </div>
     )
