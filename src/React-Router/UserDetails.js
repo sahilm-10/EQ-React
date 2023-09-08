66289b3c0570f997e5b978fdde65f1b88cc4490f
Sahil Anand Malgundkar <samalgundkar@ivp.co.in>
2023-09-08 14:56:48 +0530

    useParams used

--- src/React-Router/UserDetails.js
+++ src/React-Router/UserDetails.js
@@ -1,8 +1,11 @@
 import React from "react";
-
+import { useParams } from "react-router-dom";
 function UserDetails(){
+    // params extract the dynamic parameter we pass in. we can then use this for mapping based on userId
+    const params = useParams();
+    const userId = params.userId;
     return(
-        <div>Details about User</div>
+        <div>Details about User {userId}</div>
     )
 }
 
