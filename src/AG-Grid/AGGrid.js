47bf2d56b476ff41c210aaf9c1fc94d0ccc72bef
Sahil Anand Malgundkar <samalgundkar@ivp.co.in>
2023-09-15 15:44:10 +0530

    Added ExportAsCSV Button

--- src/AG-Grid/AGGrid.js
+++ src/AG-Grid/AGGrid.js
@@ -19,14 +19,25 @@ const AGGrid = () => {
     const defaultColDefs = {
         sortable:true , filter:true, editable:true, floatingFilter:true , flex:1
     }
+    let gridApi;
+    const onGridReady = (params) =>{
+        gridApi = params.api;
+    }
+    const exportGrid = () =>{
+        gridApi.exportDataAsCsv();
+    }
     return (
+        <>
+        <button onClick={()=>exportGrid()}>Export</button>
         <div className="ag-theme-alpine" style={{height: 500, width: '100%'}}>
             <AgGridReact
                 rowData={rowData}
                 columnDefs={columnDefs}
-                defaultColDef={defaultColDefs}>
+                defaultColDef={defaultColDefs}
+                onGridReady={onGridReady}>
             </AgGridReact>
         </div>
+        </>
     );
 };
 
