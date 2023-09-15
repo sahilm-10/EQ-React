06cea2d55b55c59dcdc52cf89ba750b4b28b0a2b
Sahil Anand Malgundkar <samalgundkar@ivp.co.in>
2023-09-15 21:11:36 +0530

    Updated TableGrid

--- src/AG-Grid/TableGridCell.js
+++ src/AG-Grid/TableGridCell.js
@@ -6,6 +6,7 @@ import 'ag-grid-community/styles/ag-grid.css';
 import 'ag-grid-community/styles/ag-theme-alpine.css';
 
 const TableGridCell = () => {
+    const[gridApi,setGridApi] = useState();
     const checkDetails = (params) =>{
         console.log(params);
         alert( `Make:${params.data.make} & Price:${params.data.price} for Model:${params.data.model}`)
@@ -35,18 +36,28 @@ const TableGridCell = () => {
         flex:1
     }
     const gridReady =(params)=>{
+        setGridApi(params);
         console.log("GridReady Function");
         fetch('https://jsonplaceholder.typicode.com/comments')
             .then(res => res.json())
             .then(res => 
-                params.api.applyTransaction({add:res}))
+            params.api.applyTransaction({add:res}))
+            params.api.paginationGoToPage(25)
     }
     const rowSelection = 'single';
     const onSelectionChanged = (event) =>{
         console.log(event.api.getSelectedRows());
     }
+    const onPaginationChange = (pageSize) =>{
+        gridApi.api.paginationSetPageSize(pageSize)
+    }
     return (
         <div className="ag-theme-alpine" style={{height: 500, width: '100%'}}>
+            <select onChange={(e)=>onPaginationChange(e.target.value)}>
+                <option value="10">10</option>
+                <option value="25">25</option>
+                <option value="50">50</option>
+            </select>
             <AgGridReact
                 // rowData={rowData}
                 columnDefs={columnDefs}
@@ -54,8 +65,11 @@ const TableGridCell = () => {
                 enableBrowserTooltips={true}
                 tooltipShowDelay={{tooltipShowDelay:2}}
                 onGridReady={gridReady}
-                rowSelection={rowSelection}
-                onSelectionChanged={onSelectionChanged}
+                // rowSelection={rowSelection}
+                // onSelectionChanged={onSelectionChanged}
+                pagination={true}
+                paginationPageSize={10}
+                // paginationAutoPageSize={true}
                 // rowMultiSelectWithClick={true}
                 >
             </AgGridReact>
