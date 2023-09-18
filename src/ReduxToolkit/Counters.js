fd5037b6048b8e3288ff131068a86f5fd17e0666
Sahil Anand Malgundkar <samalgundkar@ivp.co.in>
2023-09-18 19:36:55 +0530

    Added Counters with ReduxToolkit

--- src/ReduxToolkit/Counters.js
+++ src/ReduxToolkit/Counters.js
@@ -1,14 +1,18 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { incNum,decNum,incByAmount,decByAmount } from './action-reducers/numberChange'
+import Button from '@mui/material/Button';
+import '../ReduxToolkit/styles/button.scss'
 function Counters(){
     const dispatch = useDispatch();
     const {value} = useSelector((state)=>state.numberChange)
     return(
         <div>
-            <button onClick={()=>dispatch(incNum())}>Increment</button>
+            <h1>Increment/Decrement Counter</h1>
+            <Button className="counter-btn" variant="outlined"  onClick={()=>dispatch(incNum())}>Increment</Button>
+            {/* <Button variant="outlined">Outlined</Button> */}
             <span style={{padding:'10px'}}>{value}</span>
-            <button style={{marginBottom:'30px'}} onClick={()=>dispatch(decNum())}>Decrement</button>
+            <Button variant="outlined"  className="counter-btn" onClick={()=>dispatch(decNum())}>Decrement</Button>
         </div>
     )
 }
